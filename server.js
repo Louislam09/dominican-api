3b31e7dbc703d4579bc2bac6e81268f775a70ae8
Luis Martinez <luis.martinez@newfold.com>
2023-12-15 13:23:44 -0400

    update

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+  res.send({ msg: 'Hello World!' })
+})
+
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });
